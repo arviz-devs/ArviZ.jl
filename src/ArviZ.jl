6d03c82899fd15ffabf498413ad1325d32359211
Seth Axen <seth.axen@gmail.com>
2019-10-27 20:06:15 -0700

    Add conversion function

--- src/ArviZ.jl
+++ src/ArviZ.jl
@@ -21,7 +21,8 @@ export plot_autocorr,
     plot_ppc,
     plot_rank,
     plot_trace,
-    plot_violin
+    plot_violin,
+    convert_to_inference_data
 
 using Reexport
 using PyCall
@@ -47,6 +48,6 @@ macro delegate(f_list...)
     blocks
 end
 
-@delegate plot_autocorr plot_compare plot_density plot_dist plot_elpd plot_energy plot_ess plot_forest plot_hpd plot_joint plot_kde plot_khat plot_loo_pit plot_mcse plot_pair plot_parallel plot_posterior plot_ppc plot_rank plot_trace plot_violin
+@delegate plot_autocorr plot_compare plot_density plot_dist plot_elpd plot_energy plot_ess plot_forest plot_hpd plot_joint plot_kde plot_khat plot_loo_pit plot_mcse plot_pair plot_parallel plot_posterior plot_ppc plot_rank plot_trace plot_violin convert_to_inference_data
 
 end # module
