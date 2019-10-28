b8fe29088ce4520ee0d1cf964f40684ea46e1632
Seth Axen <seth.axen@gmail.com>
2019-10-28 02:33:46 -0700

    Add InferenceData wrapper and converters

--- src/ArviZ.jl
+++ src/ArviZ.jl
@@ -1,11 +1,14 @@
 __precompile__()
 module ArviZ
 
+using Base: display, summary
 using Reexport
 using PyCall
 @reexport using PyPlot
+using Pandas
 
-export plot_autocorr,
+export InferenceData,
+    plot_autocorr,
     plot_compare,
     plot_density,
     plot_dist,
@@ -35,6 +38,31 @@ function __init__()
     copy!(arviz, pyimport_conda("arviz", "arviz"))
 end
 
+struct InferenceData
+    o::PyObject
+end
+
+function InferenceData(args...; kwargs...)
+    data = arviz.InferenceData(args...; kwargs...)
+    return InferenceData(data)
+end
+
+InferenceData(data::InferenceData) = data
+
+Base.display(data::InferenceData) = Base.display(data.o)
+
+Base.summary(data::InferenceData) = Pandas.DataFrame(arviz.summary(data.o))
+
+convert_to_arviz_data(data) = data
+convert_to_arviz_data(data::InferenceData) = data.o
+
+function convert_to_inference_data(args...; kwargs...)
+    data = arviz.convert_to_inference_data(args...; kwargs...)
+    return InferenceData(data)
+end
+
+convert_to_inference_data(data::InferenceData) = data
+
 # Adapted from https://github.com/JuliaPy/Seaborn.jl
 macro delegate(f_list...)
     blocks = Expr(:block)
@@ -49,6 +77,6 @@ macro delegate(f_list...)
     blocks
 end
 
-@delegate plot_autocorr plot_compare plot_density plot_dist plot_elpd plot_energy plot_ess plot_forest plot_hpd plot_joint plot_kde plot_khat plot_loo_pit plot_mcse plot_pair plot_parallel plot_posterior plot_ppc plot_rank plot_trace plot_violin convert_to_inference_data
+@delegate plot_autocorr plot_compare plot_density plot_dist plot_elpd plot_energy plot_ess plot_forest plot_hpd plot_joint plot_kde plot_khat plot_loo_pit plot_mcse plot_pair plot_parallel plot_posterior plot_ppc plot_rank plot_trace plot_violin
 
 end # module
