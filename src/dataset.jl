c1453f614b8bc36e4f38d41bfd3c6fc61abf765a
Seth Axen <seth.axen@gmail.com>
2019-11-10 16:04:28 -0800

    Add function for converting dataset to dict

--- src/dataset.jl
+++ src/dataset.jl
@@ -1,2 +1,32 @@
 @forwardfun convert_to_dataset
 @forwardfun dict_to_dataset
+
+"""
+    dataset_to_dict(ds::PyObject)
+
+Convert an `xarray.Dataset` to a dictionary of `Array`s. The function is the
+returns a `Dict{String,Array}` and a `NamedTuple` with keyword arguments to
+`dict_to_dataset`.
+"""
+function dataset_to_dict(ds::PyObject)
+    ds_dict = ds.to_dict()
+    data_vars = ds_dict["data_vars"]
+    attrs = ds_dict["attrs"]
+
+    coords = ds_dict["coords"]
+    delete!(coords, "chain")
+    delete!(coords, "draw")
+    coords = Dict(k => v["data"] for (k, v) in coords)
+
+    data = Dict{String,Array}()
+    dims = Dict{String,Vector{String}}()
+    for (k, v) in data_vars
+        data[k] = v["data"]
+        dim = v["dims"][3:end]
+        if !isempty(dim)
+            dims[k] = [dim...]
+        end
+    end
+
+    return data, (attrs = attrs, coords = coords, dims = dims)
+end
