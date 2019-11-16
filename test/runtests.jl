408264ac338e7982583d49eadc02901b4a5dda0d
Seth Axen <seth.axen@gmail.com>
2019-11-16 15:44:36 -0800

    Add tests for summarize and summary

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 include("helpers.jl")
 include("test_dataset.jl")
 include("test_data.jl")
+include("test_stats.jl")
 include("test_mcmcchains.jl")
---
+++ test/test_stats.jl
@@ -0,0 +1,35 @@
+import Pandas
+
+@testset "summarize" begin
+    rng = MersenneTwister(42)
+    nchains, ndraws = 4, 10
+    idata = convert_to_inference_data(Dict(
+        "a" => randn(rng, nchains, ndraws),
+        "b" => randn(rng, nchains, ndraws, 3, 4),
+    ))
+
+    @test summarize(idata) isa Pandas.DataFrame
+    @test summarize(idata; fmt = "wide") isa Pandas.DataFrame
+    @test summarize(idata; fmt = "long") isa Pandas.DataFrame
+    s = summarize(idata)
+    @test "a" ∈ Pandas.index(s)
+    @test "b" ∉ Pandas.index(s)
+    @test "b[0,0]" ∉ Pandas.index(s)
+    @test "b[1,1]" ∈ Pandas.index(s)
+    @test "b[0,0]" ∈ Pandas.index(summarize(idata; index_origin = 0))
+
+    s2 = summarize(idata; fmt = "xarray")
+    @test s2 isa PyObject
+    @test s2.__class__.__name__ == "Dataset"
+end
+
+@testset "summary" begin
+    rng = MersenneTwister(42)
+    nchains, ndraws = 4, 10
+    idata = convert_to_inference_data(Dict(
+        "a" => randn(rng, nchains, ndraws),
+        "b" => randn(rng, nchains, ndraws, 3, 4),
+    ))
+
+    @test summary(idata) isa Pandas.DataFrame
+end
