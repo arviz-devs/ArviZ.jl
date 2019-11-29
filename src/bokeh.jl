d97850b6f4577f502131d0c0481550b36b26c0cc
Seth Axen <seth.axen@gmail.com>
2019-11-29 13:19:04 -0800

    Render in Jupyter, Juno, VSCode, and REPL

--- src/bokeh.jl
+++ src/bokeh.jl
@@ -16,3 +16,19 @@ function Base.getproperty(plot::BokehPlot, name::Symbol)
     return getproperty(o, name)
 end
 
+function render_html(plot::BokehPlot, name = nothing)
+    obj = PyObject(plot)
+    return bokeh.embed.file_html(obj, bokeh.resources.CDN, name)
+end
+
+Base.display(::REPL.REPLDisplay, plot::BokehPlot) = bokeh.plotting.show(plot)
+
+Base.show(io::IO, ::MIME"text/html", plot::BokehPlot) = print(io, render_html(plot))
+
+function Base.show(io::IO, ::MIME"application/prs.juno.plotpane+html", plot::BokehPlot)
+    return print(io, render_html(plot))
+end
+
+function Base.show(io::IO, ::MIME"juliavscode/html", plot::BokehPlot)
+    return print(io, render_html(plot))
+end
