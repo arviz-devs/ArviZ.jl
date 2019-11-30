bb2c53db55ddc1f5c6fef6b82b421af9691de0be
Seth Axen <seth.axen@gmail.com>
2019-11-30 10:03:38 -0800

    Test most of bokeh on old ArviZ version

--- test/test_backend.jl
+++ test/test_backend.jl
@@ -8,34 +8,36 @@ using PyCall
     @test plot_trace(idata) isa Array{PyObject}
 end
 
-if !ispynull(ArviZ.bokeh) && "plot.backend" in ArviZ.rc_params()
+if !ispynull(ArviZ.bokeh)
     @testset "bokeh backend" begin
         idata = load_arviz_data("centered_eight")
 
-        with_rc_context(rc = Dict("plot.backend" => "bokeh")) do
-            backend = get(ArviZ.rc_params(), "plot.backend", nothing)
-            @test backend == "bokeh"
+        plot = plot_trace(idata; backend = "bokeh")
 
-            @test plot_trace(idata) isa ArviZ.BokehPlot
-            @test plot_trace(idata; backend = "matplotlib") isa Array{PyObject}
+        @test plot isa ArviZ.BokehPlot
 
-            @testset "ArviZ.BokehPlot" begin
-                plot = plot_trace(idata)
+        pyobj = PyObject(plot)
+        plot2 = convert(ArviZ.BokehPlot, pyobj)
+        @test plot2 isa ArviZ.BokehPlot
+        @test hash(plot) == hash(plot2)
+        @test pyobj === PyObject(plot2)
 
-                pyobj = PyObject(plot)
-                plot2 = convert(ArviZ.BokehPlot, pyobj)
-                @test plot2 isa ArviZ.BokehPlot
-                @test hash(plot) == hash(plot2)
-                @test pyobj === PyObject(plot2)
+        @test propertynames(plot) == propertynames(PyObject(plot))
+        getproperty(plot, :__class__)
 
-                @test propertynames(plot) == propertynames(PyObject(plot))
-                getproperty(plot, :__class__)
+        @testset "MIME::\"$(mime)\"" for mime in ["text/html",]
+            text = repr(MIME(mime), plot)
+            @test text isa String
+            @test occursin("<body", text)
+        end
 
-                @testset "MIME::\"$(mime)\"" for mime in ["text/html",]
-                    text = repr(MIME(mime), plot)
-                    @test text isa String
-                    @test occursin("<body", text)
-                end
+        if "plot.backend" in ArviZ.rc_params()
+            with_rc_context(rc = Dict("plot.backend" => "bokeh")) do
+                backend = ArviZ.rc_params()["plot.backend"]
+                @test backend == "bokeh"
+
+                @test plot_trace(idata) isa ArviZ.BokehPlot
+                @test plot_trace(idata; backend = "matplotlib") isa Array{PyObject}
             end
         end
     end
