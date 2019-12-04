2dab5f75c2029b1196298232a76df625935e798a
Seth Axen <seth.axen@gmail.com>
2019-12-04 01:23:25 -0800

    Add custom methods for particles

--- src/particles.jl
+++ src/particles.jl
@@ -1,2 +1,16 @@
 import .MonteCarloMeasurements: AbstractParticles
 
+stack(x::AbstractParticles) = Array(x)
+
+function stack(v::AbstractArray{<:AbstractParticles})
+    m = reduce(hcat, stack.(v))
+    return Array(reshape(m, size(m, 1), size(v)...))
+end
+
+function convert_to_inference_data(data::AbstractParticles; kwargs...)
+    return convert_to_inference_data(stack(data); kwargs...)
+end
+
+function convert_to_inference_data(data::AbstractArray{<:AbstractParticles}; kwargs...)
+    return convert_to_inference_data(stack(data); kwargs...)
+end
