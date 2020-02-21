94f6dc95a906c82910a88f7d08b6e696b25c05da
Seth Axen <seth.axen@gmail.com>
2020-02-20 16:49:47 -0800

    Run all tests in super block

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using ArviZ
 using Test
 
-include("helpers.jl")
-include("test_backend.jl")
-include("test_dataset.jl")
-include("test_data.jl")
-include("test_stats.jl")
-include("test_namedtuple.jl")
-include("test_mcmcchains.jl")
+@testset "ArviZ" begin
+    include("helpers.jl")
+    include("test_backend.jl")
+    include("test_dataset.jl")
+    include("test_data.jl")
+    include("test_stats.jl")
+    include("test_namedtuple.jl")
+    include("test_mcmcchains.jl")
+end
