7a9a58edf43f54c35c4de2f533ec6c8104cc4032
Seth Axen <seth.axen@gmail.com>
2020-02-26 03:39:11 -0800

    Fix some conversions

--- src/plots.jl
+++ src/plots.jl
@@ -22,11 +22,6 @@
 
 # TODO: Add conversions for plot_compare, plot_elpd, and plot_khat
 
-function convert_arguments(::typeof(plot_trace), data, args...; kwargs...)
-    idata = convert_to_inference_data(data; group = :posterior)
-    return tuple(idata, args...), kwargs
-end
-
 for f in (
     :plot_autocorr,
     :plot_ess,
@@ -34,19 +29,19 @@ for f in (
     :plot_mcse,
     :plot_pair,
     :plot_posterior,
+    :plot_trace,
     :plot_violin,
 )
     @eval begin
-        function convert_arguments(
-            ::typeof($(f)),
-            data,
-            args...;
-            group = :posterior,
-            kwargs...,
-        )
-            ds = convert_to_dataset(data; group = group)
-            return tuple(ds, args...), kwargs
+        function convert_arguments(::typeof($(f)), data, args...; kwargs...)
+            idata = convert_to_inference_data(data; group = :posterior)
+            return tuple(idata, args...), kwargs
         end
+    end
+end
+
+for f in (:plot_autocorr, :plot_ess, :plot_mcse, :plot_posterior, :plot_violin)
+    @eval begin
         function convert_arguments(::typeof($(f)), data::AbstractArray, args...; kwargs...)
             return tuple(data, args...), kwargs
         end
@@ -58,9 +53,6 @@ for f in (:plot_energy, :plot_parallel)
         function convert_arguments(::typeof($(f)), data, args...; kwargs...)
             dataset = convert_to_dataset(data; group = :sample_stats)
             return tuple(dataset, args...), kwargs
-        end
-        function convert_arguments(::typeof($(f)), data::AbstractArray, args...; kwargs...)
-            return tuple(data, args...), kwargs
         end
     end
 end
@@ -97,6 +89,18 @@ for f in (:plot_density, :plot_forest)
                 return convert_to_dataset(datum; group = group)
             end
             return tuple(datasets, args...), kwargs
+        end
+        function convert_arguments(
+            ::typeof($(f)),
+            data::AbstractVector{<:Real},
+            transform = identity,
+            group = :posterior,
+            args...;
+            kwargs...,
+        )
+            tdata = transform(data)
+            dataset = convert_to_dataset(tdata; group = group)
+            return tuple(dataset, args...), kwargs
         end
     end
 end
