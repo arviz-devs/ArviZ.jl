500f41bd0e058aa3e9c702e69533d269d5f7b777
Seth Axen <seth.axen@gmail.com>
2020-02-28 00:14:11 -0800

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "ArviZ" begin
     include("helpers.jl")
     include("test_rcparams.jl")
+    include("test_utils.jl")
     include("test_backend.jl")
     include("test_dataset.jl")
     include("test_data.jl")
