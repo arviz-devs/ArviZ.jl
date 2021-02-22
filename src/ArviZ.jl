0fd13cbc34653ef09b89af915e8f992240d061df
Seth Axen <seth.axen@gmail.com>
2021-02-21 18:28:16 -0800

    Bump required arviz version
    Earliest version that includes the scheme update, just to be safe.

--- src/ArviZ.jl
+++ src/ArviZ.jl
@@ -85,7 +85,7 @@ export with_interactive_backend
 ## rcParams
 export rcParams, with_rc_context
 
-const _min_arviz_version = v"0.10.0"
+const _min_arviz_version = v"0.11.0"
 const arviz = PyNULL()
 const xarray = PyNULL()
 const bokeh = PyNULL()
