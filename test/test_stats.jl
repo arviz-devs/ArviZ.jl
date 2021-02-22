bbdf6376f7e301a405ad8e34f162f25c01234a63
Seth Axen <seth.axen@gmail.com>
2021-02-21 17:45:14 -0800

    Test both old and new variable formats

--- test/test_stats.jl
+++ test/test_stats.jl
@@ -63,9 +63,10 @@ using DataFrames: DataFrames
         @test :variable in propertynames(summarystats(idata; fmt="wide"))
         @test "a" ∈ s.variable
         @test "b" ∉ s.variable
-        @test "b[0,0]" ∉ s.variable
-        @test "b[1,1]" ∈ s.variable
-        @test "b[0,0]" ∈ summarystats(idata; index_origin=0).variable
+        @test "b[0,0]" ∉ s.variable && "b[0, 0]" ∉ s.variable
+        @test "b[1,1]" ∈ s.variable || "b[1, 1]" ∈ s.variable
+        var_summary = summarystats(idata; index_origin=0).variable
+        @test "b[0,0]" ∈ var_summary || "b[0, 0]" ∈ var_summary
 
         s2 = summarystats(idata; fmt="long")
         @test s2 isa DataFrames.DataFrame
