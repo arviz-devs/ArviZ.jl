5f5f7fd8cbf13c4998477f32b372f205b36dd6c1
Seth Axen <seth.axen@gmail.com>
2022-03-31 23:42:01 +0200

    Make the Quickstart docs a Pluto notebook (#136)
    * Add Pluto as docs dependency
    
    * Add quickstart as Pluto notebook
    
    * Build notebook during docs
    
    * Embed notebook as iframe
    
    * Remove now-unused dependencies
    
    * Commit manifest
    
    to use Pluto#main
    
    * Update docs/notebooks/quickstart.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Instantiate before dev
    
    This prevents errors when using the manifest.
    
    * Increment version number
    
    * Build notebook in assets folder
    
    Because directory structure is different locally than remotely
    
    * Revert "Build notebook in assets folder"
    
    This reverts commit de8b8b3ebe3c19396e35bcb322789c5ecc207ebf.
    
    * Correctly point to quickstart notebook
    
    Only works remotely, not locally
    
    * Revert "Commit manifest"
    
    This reverts commit 67733e918d7cfb209a764a405860d1c780a7cb69.
    
    * Revert "Instantiate before dev"
    
    This reverts commit 1065e1a698353698daafab59720bdc7e7f35a394.
    
    * Use release Pluto
    
    * Update Soss examples in notebook
    
    * Run formatter
    
    * Remove unneeded dependency
    
    * Remove unused docs dependencies
    
    * Remove quickstart wrapper
    
    * Move notebooks to src
    
    * Build quickstart with PlutoStaticHTML
    
    * Run formatter
    
    * Allow piping syntax used by Soss
    
    * Improvements to notebook formatting
    
    * Fix formatting
    
    * Remove notebooks directory for now
    
    * Add custom CSS
    
    * Fix quickstart link
    
    * Add header links for backward compatibility
    
    * Increment patch version
    
    * Improve top padding
    
    * Remove Pluto as dependency
    
    * Use docs environment in notebook
    
    * Add back PlutoStaticHTML as dependency
    
    * Use with_terminal
    
    * Add PlutoUI compat
    
    * Increment patch version number
    
    * Load Pkg separately
    
    * Remove unused color information
    
    * Use post-1.6 simplified keyword syntax
    
    * Override PlutoStaticHTML's margin settings
    
    * Increment patch version
    
    * Add missing comma
    
    * Run formatter
    
    * Opt out of PlutoStaticHTML's custom css
    
    * Improve admonition css
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,19 @@
 using Documenter, Downloads, ArviZ
 using MCMCChains: MCMCChains # make `from_mcmcchains` available for API docs
 using SampleChains: SampleChains # make `from_samplechains` available for API docs
+using PlutoStaticHTML: PlutoStaticHTML
+
+const DOCS_SRC_PATH = joinpath(@__DIR__, "src")
+
+# generate markdown from Pluto notebooks
+output_format = PlutoStaticHTML.documenter_output
+build_opts = PlutoStaticHTML.BuildOptions(
+    DOCS_SRC_PATH;
+    previous_dir=DOCS_SRC_PATH,
+    output_format=output_format,
+    add_documenter_css=false,
+)
+PlutoStaticHTML.build_notebooks(build_opts)
 
 const ASSETS_DIR = joinpath(@__DIR__, "src", "assets")
 const ARVIZ_ASSETS_URL = "https://raw.githubusercontent.com/arviz-devs/arviz_governance/main/sphinx"
@@ -31,7 +44,7 @@ makedocs(;
     checkdocs=:exports,
     format=Documenter.HTML(;
         prettyurls=haskey(ENV, "CI"),
-        assets=["assets/favicon.ico"],
+        assets=["assets/favicon.ico", "assets/custom.css"],
         sidebar_sitename=false,
         canonical="stable",
     ),
