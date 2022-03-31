d9b62516d527e1d8a6d5c640ec322176f1edbd55
Seth Axen <seth.axen@gmail.com>
2022-03-31 19:00:02 +0200

    Fix formatting

--- docs/make.jl
+++ docs/make.jl
@@ -31,7 +31,7 @@ makedocs(;
     checkdocs=:exports,
     format=Documenter.HTML(;
         prettyurls=haskey(ENV, "CI"),
-        assets=[asset("assets/favicon.png", class=:ico, islocal = true)],
+        assets=[asset("assets/favicon.png"; class=:ico, islocal=true)],
         sidebar_sitename=false,
         canonical="stable",
     ),
