b9e440d7d5f016a0eccd8d19779f6f2f6049c95c
Seth Axen <seth.axen@gmail.com>
2022-03-31 23:26:24 +0200

    Actually grab favicon.ico

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ end
 # download arviz-devs org logo assets
 download_asset("ArviZ.png", "logo.png")
 download_asset("ArviZ_white.png", "logo-dark.png")
-download_asset("ArviZ_fav.png", "favicon.png")
+download_asset("favicon.io")
 
 makedocs(;
     modules=[ArviZ],
@@ -31,7 +31,7 @@ makedocs(;
     checkdocs=:exports,
     format=Documenter.HTML(;
         prettyurls=haskey(ENV, "CI"),
-        assets=["assets/favicon.png", "assets/custom.css"],
+        assets=["assets/favicon.io", "assets/custom.css"],
         sidebar_sitename=false,
         canonical="stable",
     ),
