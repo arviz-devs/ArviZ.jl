ca35102fbaa7f669a6e282d990cb4aeeb953bc20
Seth Axen <seth.axen@gmail.com>
2022-07-25 21:17:43 +0200

    Reorg Dataset docstring

--- src/dataset.jl
+++ src/dataset.jl
@@ -1,16 +1,7 @@
 """
     Dataset{L} <: DimensionalData.AbstractDimStack{L}
 
-    Dataset(data::DimensionalData.AbstractDimArray...)
-    Dataset(data::Tuple{Vararg{<:DimensionalData.AbstractDimArray}})
-    Dataset(data::NamedTuple{Keys,Vararg{<:DimensionalData.AbstractDimArray}})
-    Dataset(
-        data::NamedTuple,
-        dims::Tuple{Vararg{DimensionalData.Dimension}};
-        metadata=DimensionalData.NoMetadata(),
-    )
-
-Collection of dimensional arrays with shared dimensions.
+Container of dimensional arrays sharing some dimensions.
 
 This type is an
 [`DimensionalData.AbstractDimStack`](https://rafaqz.github.io/DimensionalData.jl/stable/api/#DimensionalData.AbstractDimStack)
@@ -19,6 +10,17 @@ that implements the same interface as `DimensionalData.DimStack` and has identic
 When a `Dataset` is passed to Python, it is converted to an `xarray.Dataset` without copying
 the data. That is, the Python object shares the same memory as the Julia object. However,
 if an `xarray.Dataset` is passed to Julia, its data must be copied.
+
+# Constructors
+
+    Dataset(data::DimensionalData.AbstractDimArray...)
+    Dataset(data::Tuple{Vararg{<:DimensionalData.AbstractDimArray}})
+    Dataset(data::NamedTuple{Keys,Vararg{<:DimensionalData.AbstractDimArray}})
+    Dataset(
+        data::NamedTuple,
+        dims::Tuple{Vararg{DimensionalData.Dimension}};
+        metadata=DimensionalData.NoMetadata(),
+    )
 
 In most cases, use [`convert_to_dataset`](@ref) to create a `Dataset` instead of directly
 using a constructor.
