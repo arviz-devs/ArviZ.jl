42010d51550bdb23cbc5cf2c4c330ab2de65fc8c
Seth Axen <seth.axen@gmail.com>
2022-07-27 01:57:10 +0200

    Remove undefined keyword

--- deps/build.jl
+++ deps/build.jl
@@ -3,5 +3,5 @@ using Conda
 # try to install scipy with pip if not yet installed
 # temporary workaround for https://github.com/arviz-devs/ArviZ.jl/issues/188
 Conda.pip_interop(true)
-Conda.pip("uninstall -y --no-deps", "scipy")
+Conda.pip("uninstall -y", "scipy")
 Conda.pip("install", "scipy")
