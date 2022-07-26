a69b99c5a6d5cbb50e437132d34a6abbd5cdcc17
Seth Axen <seth.axen@gmail.com>
2022-07-27 01:28:03 +0200

    Don't install deps

--- deps/build.jl
+++ deps/build.jl
@@ -2,4 +2,4 @@ using PyCall
 
 # try to install scipy with pip if not yet installed
 # temporary workaround for https://github.com/arviz-devs/ArviZ.jl/issues/188
-run(PyCall.python_cmd(`-m pip install --force-reinstall -- scipy`))
+run(PyCall.python_cmd(`-m pip install --force-reinstall --no-deps scipy`))
