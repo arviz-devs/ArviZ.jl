407039011ca9d9b2bb219d176e1d86d6be94e202
Seth Axen <seth.axen@gmail.com>
2022-07-27 07:06:17 +0200

    Install scipy dependency from pypi (#202)
    * Install scipy via pip if not installed
    
    * Remove LD_PRELOAD from CI
    
    * Pip install scipy during build phase
    
    * Don't install deps
    
    * Try removing scipy before installing
    
    * Remove undefined keyword
    
    * Explicitly build package before docs

---
+++ deps/build.jl
@@ -0,0 +1,7 @@
+using Conda
+
+# try to install scipy with pip if not yet installed
+# temporary workaround for https://github.com/arviz-devs/ArviZ.jl/issues/188
+Conda.pip_interop(true)
+Conda.pip("uninstall -y", "scipy")
+Conda.pip("install", "scipy")
