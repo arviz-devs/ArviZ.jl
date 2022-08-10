662f2de649a314878a5d375f0e9aaea0ba5caa11
Seth Axen <seth.axen@gmail.com>
2022-08-10 23:47:26 +0200

    Separate out dataset conversion tests

---
+++ test/InferenceObjects/convert_dataset.jl
@@ -0,0 +1,34 @@
+using ArviZ.InferenceObjects, DimensionalData, Test
+
+@testset "conversion to Dataset" begin
+    @testset "convert_to_dataset" begin
+        nchains = 4
+        ndraws = 100
+        nshared = 3
+        xdims = (:chain, :draw, :shared)
+        x = DimArray(randn(nchains, ndraws, nshared), xdims)
+        ydims = (:chain, :draw, Dim{:ydim1}(Any["a", "b"]), Dim{:shared})
+        y = DimArray(randn(nchains, ndraws, 2, nshared), ydims)
+        metadata = Dict(:prop1 => "val1", :prop2 => "val2")
+        ds = ArviZ.Dataset((; x, y); metadata)
+
+        @testset "ArviZ.convert_to_dataset(::ArviZ.Dataset; kwargs...)" begin
+            @test ArviZ.convert_to_dataset(ds) isa ArviZ.Dataset
+            @test ArviZ.convert_to_dataset(ds) === ds
+        end
+
+        @testset "ArviZ.convert_to_dataset(::$T; kwargs...)" for T in (Dict, NamedTuple)
+            data = (x=randn(4, 100), y=randn(4, 100, 2))
+            if T <: Dict
+                data = T(pairs(data))
+            end
+            ds2 = ArviZ.convert_to_dataset(data)
+            @test ds2 isa ArviZ.Dataset
+            @test ds2.x == data[:x]
+            @test DimensionalData.name(DimensionalData.dims(ds2.x)) == (:chain, :draw)
+            @test ds2.y == data[:y]
+            @test DimensionalData.name(DimensionalData.dims(ds2.y)) ==
+                (:chain, :draw, :y_dim_1)
+        end
+    end
+end
--- test/InferenceObjects/runtests.jl
+++ test/InferenceObjects/runtests.jl
@@ -3,4 +3,5 @@ using ArviZ.InferenceObjects, Test
 @testset "InferenceObjects" begin
     include("dataset.jl")
     include("inference_data.jl")
+    include("convert_dataset.jl")
 end
