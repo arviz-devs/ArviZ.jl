38f086f62ecf2ea5a0d375b907d92f614179f302
Seth Axen <seth.axen@gmail.com>
2022-08-10 22:52:33 +0200

    Reorganize files into submodule

--- src/InferenceObjects/InferenceObjects.jl
+++ src/InferenceObjects/InferenceObjects.jl
@@ -1,3 +1,11 @@
 module InferenceObjects
 
+using DimensionalData: DimensionalData, Dimensions
+
+export Dataset, InferenceData
+export convert_to_dataset, convert_to_inference_data, namedtuple_to_dataset
+
+include("dataset.jl")
+include("inference_data.jl")
+
 end # module
