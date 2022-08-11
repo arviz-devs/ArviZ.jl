f5c6a75d9549c83130a46cfba96b3e06d62e7f71
Seth Axen <seth.axen@gmail.com>
2022-08-11 13:32:35 +0200

    Reorganize code

--- src/InferenceObjects/dimensions.jl
+++ src/InferenceObjects/dimensions.jl
@@ -1,67 +1,4 @@
-
-"""
-    array_to_dimarray(array, name; kwargs...) -> DimensionalData.AbstractDimArray
-
-Convert `array` to a `AbstractDimArray` with name `name`.
-
-If `array` is already an `AbstractDimArray`, then it is returned without modification.
-See [`generate_dims`](@ref) for a description of `kwargs`.
-"""
-function array_to_dimarray end
-function array_to_dimarray(array::DimensionalData.AbstractDimArray, name; kwargs...)
-    return DimensionalData.rebuild(array; name)
-end
-function array_to_dimarray(data, name; dims=(), coords=(;), default_dims=())
-    array = if ndims(data) < 2 && has_all_sample_dims(default_dims)
-        reshape(data, 1, :)
-    else
-        data
-    end
-    array_dims = generate_dims(array, name; dims, coords, default_dims)
-    return DimensionalData.DimArray(array, array_dims; name)
-end
-
 has_all_sample_dims(dims) = all(Dimensions.hasdim(dims, DEFAULT_SAMPLE_DIMS))
-
-"""
-    generate_dims(array, name; dims, coords, default_dims)
-
-Generate `DimensionsionalData.Dimension` objects for each dimension of `array`.
-
-`name` indicates the name of the variable represented by array.
-
-# Keywords
-
-  - `dims`: A collection of objects indicating dimension names. If any dimensions are not
-    provided, their names are automatically generated. Acceptable types of entries are:
-    
-      + `Symbol`: dimension name
-      + `Type{<:DimensionsionalData.Dimension}`: dimension type
-      + `DimensionsionalData.Dimension`: dimension, potentially with indices
-      + `Nothing`: no dimension name provided, dimension name is automatically generated
-
-  - `coords`: a collection indexable by dimension name specifying the indices of the given
-    dimension. If indices for a dimension in `dims` are provided, they are used even if
-    the dimension contains its own indices. If a dimension is missing, its indices are
-    automatically generated.
-  - `default_dims`: A collection of dims to be prepended to `dims` whose elements have the
-    same constraints.
-"""
-function generate_dims end
-function generate_dims(array, name; dims=(), coords=(;), default_dims=())
-    num_default_dims = length(default_dims)
-    length(dims) + num_default_dims > ndims(array) && @error "blah"
-    dims_named = ntuple(ndims(array) - length(default_dims)) do i
-        dim = get(dims, i, nothing)
-        dim === nothing && return Symbol("$(name)_dim_$(i)")
-        return dim
-    end
-    dims_all = (default_dims..., dims_named...)
-    dims_with_coords = ntuple(ndims(array)) do i
-        return as_dimension(dims_all[i], coords, size(array, i))
-    end
-    return Dimensions.format(dims_with_coords, array)
-end
 
 """
     as_dimension(dim, coords, length) -> DimensionsionalData.Dimension
@@ -98,14 +35,74 @@ function as_dimension(dim::Symbol, coords, n)
 end
 
 """
-    index_to_indices(index)
+    generate_dims(array, name; dims, coords, default_dims)
 
-Convert `index` to a collection of indices or a selector representing such a collection.
+Generate `DimensionsionalData.Dimension` objects for each dimension of `array`.
+
+`name` indicates the name of the variable represented by array.
+
+# Keywords
+
+  - `dims`: A collection of objects indicating dimension names. If any dimensions are not
+    provided, their names are automatically generated. Acceptable types of entries are:
+    
+      + `Symbol`: dimension name
+      + `Type{<:DimensionsionalData.Dimension}`: dimension type
+      + `DimensionsionalData.Dimension`: dimension, potentially with indices
+      + `Nothing`: no dimension name provided, dimension name is automatically generated
+
+  - `coords`: a collection indexable by dimension name specifying the indices of the given
+    dimension. If indices for a dimension in `dims` are provided, they are used even if
+    the dimension contains its own indices. If a dimension is missing, its indices are
+    automatically generated.
+  - `default_dims`: A collection of dims to be prepended to `dims` whose elements have the
+    same constraints.
 """
-index_to_indices(i) = i
-index_to_indices(i::Int) = [i]
-index_to_indices(sel::Dimensions.Selector) = AsSlice(sel)
+function generate_dims(array, name; dims=(), coords=(;), default_dims=())
+    num_default_dims = length(default_dims)
+    length(dims) + num_default_dims > ndims(array) && @error "blah"
+    dims_named = ntuple(ndims(array) - length(default_dims)) do i
+        dim = get(dims, i, nothing)
+        dim === nothing && return Symbol("$(name)_dim_$(i)")
+        return dim
+    end
+    dims_all = (default_dims..., dims_named...)
+    dims_with_coords = ntuple(ndims(array)) do i
+        return as_dimension(dims_all[i], coords, size(array, i))
+    end
+    return Dimensions.format(dims_with_coords, array)
+end
 
+"""
+    array_to_dimarray(array, name; kwargs...) -> DimensionalData.AbstractDimArray
+
+Convert `array` to a `AbstractDimArray` with name `name`.
+
+If `array` is already an `AbstractDimArray`, then it is returned without modification.
+See [`generate_dims`](@ref) for a description of `kwargs`.
+"""
+function array_to_dimarray(data, name; dims=(), coords=(;), default_dims=())
+    array = if ndims(data) < 2 && has_all_sample_dims(default_dims)
+        reshape(data, 1, :)
+    else
+        data
+    end
+    array_dims = generate_dims(array, name; dims, coords, default_dims)
+    return DimensionalData.DimArray(array, array_dims; name)
+end
+function array_to_dimarray(array::DimensionalData.AbstractDimArray, name; kwargs...)
+    return DimensionalData.rebuild(array; name)
+end
+
+"""
+    AsSlice{T<:Dimensions.Selector} <: Dimensions.Selector{T}
+
+    AsSlice(selector)
+
+Selector that ensures selected indices are arrays so that slicing occurs.
+
+This is useful to ensure that selecting a single index still returns an array.
+"""
 struct AsSlice{T<:Dimensions.Selector} <: Dimensions.Selector{T}
     val::T
 end
@@ -115,3 +112,12 @@ function Dimensions.selectindices(l::Dimensions.LookupArray, sel::AsSlice; kw...
     inds = i isa AbstractVector ? i : [i]
     return inds
 end
+
+"""
+    index_to_indices(index)
+
+Convert `index` to a collection of indices or a selector representing such a collection.
+"""
+index_to_indices(i) = i
+index_to_indices(i::Int) = [i]
+index_to_indices(sel::Dimensions.Selector) = AsSlice(sel)
