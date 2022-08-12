98399c73b737e51f6a5e9f90cdafe053c49d0f0f
Seth Axen <seth.axen@gmail.com>
2022-08-13 01:10:16 +0200

    Run formatter

--- src/ArviZ.jl
+++ src/ArviZ.jl
@@ -36,7 +36,8 @@ import PyCall: PyObject
 
 include("InferenceObjects/InferenceObjects.jl")
 using .InferenceObjects
-using .InferenceObjects: attributes, flatten, groupnames, groups, hasgroup, rekey, setattribute!
+using .InferenceObjects:
+    attributes, flatten, groupnames, groups, hasgroup, rekey, setattribute!
 import .InferenceObjects: convert_to_inference_data
 
 # Exports
