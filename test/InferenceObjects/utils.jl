0bf195c018af4a368aafe4dc85c9249d64241f14
Seth Axen <seth.axen@gmail.com>
2022-08-13 15:08:37 +0200

    Revert "Move rekey to InferenceObjects"
    This reverts commit 6e736e421fd22acd9049af0fc5befd0a19dc3a21.

--- test/InferenceObjects/utils.jl
+++ test/InferenceObjects/utils.jl
@@ -4,19 +4,3 @@ using ArviZ.InferenceObjects
 @testset "package_version" begin
     @test InferenceObjects.package_version(ArviZ) isa VersionNumber
 end
-
-@testset "rekey" begin
-    orig = (x=3, y=4, z=5)
-    keymap = (x=:y, y=:a)
-    @testset "NamedTuple" begin
-        new = @inferred NamedTuple InferenceObjects.rekey(orig, keymap)
-        @test new isa NamedTuple
-        @test new == (y=3, a=4, z=5)
-    end
-    @testset "Dict" begin
-        orig_dict = Dict(pairs(orig))
-        new = @inferred InferenceObjects.rekey(orig_dict, keymap)
-        @test new isa typeof(orig_dict)
-        @test new == Dict(:y => 3, :a => 4, :z => 5)
-    end
-end
