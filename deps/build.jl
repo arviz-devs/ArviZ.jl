0fd16f69bc5b98f622a0dc24ea16d24cfb6ae19d
Seth Axen <seth@sethaxen.com>
2022-10-01 15:22:28 +0200

    Pin versions using conda

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,5 @@
 using Conda
 
-# try to install scipy with pip if not yet installed
 # temporary workaround for https://github.com/arviz-devs/ArviZ.jl/issues/188
-Conda.pip_interop(true)
-Conda.pip("uninstall -y", "scipy")
-Conda.pip("install", "scipy")
+Conda.add("scipy<=1.8.0")
+Conda.add("matplotlib<3.6.0")
