31aa76e3b4d2cbf2b484a10dc623319d671468d8
Seth Axen <seth@sethaxen.com>
2022-10-25 17:54:37 +0200

    Add from_beanmachine forward

--- src/data.jl
+++ src/data.jl
@@ -10,6 +10,7 @@ end
 @forwardfun from_netcdf
 @forwardfun from_json
 @forwardfun from_dict
+@forwardfun from_beanmachine
 @forwardfun from_cmdstan
 @forwardfun from_cmdstanpy
 @forwardfun from_emcee
