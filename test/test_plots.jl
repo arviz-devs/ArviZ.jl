1c9e79ecdc7078f96795a9b9f761b0ee60fa7b75
Seth Axen <seth@sethaxen.com>
2022-10-26 15:18:27 +0200

    Revert "Mark plot_kde bokeh test as broken"
    This reverts commit e64f153855173f67c97dbc1c01ddd8a0644d7fd1.

--- test/test_plots.jl
+++ test/test_plots.jl
@@ -120,7 +120,7 @@ using PyCall, PyPlot
         plot_kde(arr1)
         close(gcf())
         ispynull(ArviZ.bokeh) || @testset "bokeh" begin
-            @test_broken plot_kde(arr1; backend=:bokeh) isa ArviZ.BokehPlot
+            @test plot_kde(arr1; backend=:bokeh) isa ArviZ.BokehPlot
         end
 
         plot_kde(arr1, arr2)
