a5f37af78facfb55bd35124d9bae8a13332586cc
Seth Axen <seth@sethaxen.com>
2022-11-02 13:16:09 +0100

    Check against Python ArviZ with permuted dims

--- test/test_diagnostics.jl
+++ test/test_diagnostics.jl
@@ -11,28 +11,28 @@
 
     @testset "ess" begin
         rng = Random.MersenneTwister(42)
-        arr = randn(rng, 4, 100)
+        arr = randn(rng, ndraws, nchains)
         @test ess(arr) == ArviZ.arviz.ess(arr)
         @test ess((x=arr,)) isa ArviZ.Dataset
         @test ess((x=arr,))[:x] ==
-            pycall(ArviZ.arviz.ess, PyObject, Dict(:x => arr)).x.values
+            pycall(ArviZ.arviz.ess, PyObject, Dict(:x => permutedims(arr))).x.values
     end
 
     @testset "mcse" begin
         rng = Random.MersenneTwister(42)
-        arr = randn(rng, 4, 100)
+        arr = randn(rng, ndraws, nchains)
         @test mcse(arr) == ArviZ.arviz.mcse(arr)
         @test mcse((x=arr,)) isa ArviZ.Dataset
         @test mcse((x=arr,))[:x] ==
-            pycall(ArviZ.arviz.mcse, PyObject, Dict(:x => arr)).x.values
+            pycall(ArviZ.arviz.mcse, PyObject, Dict(:x => permutedims(arr))).x.values
     end
 
     @testset "rhat" begin
         rng = Random.MersenneTwister(42)
-        arr = randn(rng, 4, 100)
+        arr = randn(rng, ndraws, nchains)
         @test rhat(arr) == ArviZ.arviz.rhat(arr)
         @test rhat((x=arr,)) isa ArviZ.Dataset
         @test rhat((x=arr,))[:x] ==
-            pycall(ArviZ.arviz.rhat, PyObject, Dict(:x => arr)).x.values
+            pycall(ArviZ.arviz.rhat, PyObject, Dict(:x => permutedims(arr))).x.values
     end
 end
