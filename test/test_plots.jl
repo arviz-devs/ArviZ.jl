b20a078e5c532fb6d1aea4310c7c235cd67166da
Seth Axen <seth@sethaxen.com>
2022-12-30 12:51:51 +0100

    Skip plot_kde test on v1.6

--- test/test_plots.jl
+++ test/test_plots.jl
@@ -80,7 +80,7 @@ using PyCall, PyPlot
         close(gcf())
     end
 
-    @testset "plot_kde" begin
+    VERSION > v"1.6" && @testset "plot_kde" begin
         plot_kde(arr1)
         close(gcf())
 
