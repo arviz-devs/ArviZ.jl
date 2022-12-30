58622256da81a99be255710843b1410919307672
Seth Axen <seth@sethaxen.com>
2022-12-30 12:02:01 +0100

    Pin just scipy version

--- deps/build.jl
+++ deps/build.jl
@@ -3,4 +3,4 @@ using Conda
 # temporary workaround for
 # - https://github.com/arviz-devs/ArviZ.jl/issues/188
 # - https://github.com/arviz-devs/arviz/issues/2120
-Conda.add(["matplotlib<3.6.0", "pandas<1.5.0", "scipy<=1.8.0"])
+Conda.add(["scipy<=1.8.0"])
