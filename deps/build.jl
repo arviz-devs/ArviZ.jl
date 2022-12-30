b20d0f6153d0071e1b674876074cfadb0ab61b2d
Seth Axen <seth@sethaxen.com>
2022-12-30 11:47:45 +0100

    Remove deps/build.jl

--- deps/build.jl
+++
@@ -1,6 +0,0 @@
-using Conda
-
-# temporary workaround for
-# - https://github.com/arviz-devs/ArviZ.jl/issues/188
-# - https://github.com/arviz-devs/arviz/issues/2120
-Conda.add(["matplotlib<3.6.0", "pandas<1.5.0", "scipy<=1.8.0"])
