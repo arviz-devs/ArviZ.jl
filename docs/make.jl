388356701b58c37098d6729b7c5929ae5dbb6272
Seth Axen <seth@sethaxen.com>
2023-07-06 13:14:15 +0200

    Post-process html in Quarto outputs

--- docs/make.jl
+++ docs/make.jl
@@ -11,6 +11,36 @@ CondaPkg.withenv() do
     run(`quarto render $(DOCS_SRC_PATH)`)
 end
 
+function wrap_html_divs_in_raw_block(out_io, in_io)
+    level = 0
+    for line in eachline(in_io)
+        if contains(line, "<div")
+            level == 0 && write(out_io, "```@raw html\n")
+        end
+        write(out_io, line, '\n')
+        level += count("<div", line) - count("</div>", line)
+        if contains(line, "</div")
+            level == 0 && write(out_io, "```\n")
+        end
+    end
+    return nothing
+end
+
+# workaround for Julia markdown not supporting HTML
+for fn in readdir(DOCS_SRC_PATH; join=true)
+    root, ext = splitext(fn)
+    ext == ".qmd" || continue
+    md_file = root * ".md"
+    if isfile(md_file)
+        md_file_new, out_io = mktemp(; cleanup=false)
+        open(md_file, "r") do in_io
+            wrap_html_divs_in_raw_block(out_io, in_io)
+        end
+        close(out_io)
+        mv(md_file_new, md_file; force=true)
+    end
+end
+
 const ASSETS_DIR = joinpath(@__DIR__, "src", "assets")
 const ARVIZ_ASSETS_URL = "https://raw.githubusercontent.com/arviz-devs/arviz-project/main/arviz_logos"
 
