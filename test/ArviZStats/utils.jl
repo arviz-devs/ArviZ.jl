7c78dc48b6e3b8ec6c25b6eb2aa6c526305c5167
Seth Axen <seth@sethaxen.com>
2023-08-01 22:06:46 +0200

    Add formatter tests

--- test/ArviZStats/utils.jl
+++ test/ArviZStats/utils.jl
@@ -330,4 +330,83 @@ using StatsBase
         @test ArviZStats.sigdigits_matching_se(100, 0) == 7
         @test ArviZStats.sigdigits_matching_se(100, 0; sigdigits_max=2) == 2
     end
+
+    @testset "_printf_with_sigdigits" begin
+        @test ArviZStats._printf_with_sigdigits(123.456, 1) == "1.e+02"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 1) == "-1.e+02"
+        @test ArviZStats._printf_with_sigdigits(123.456, 2) == "1.2e+02"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 2) == "-1.2e+02"
+        @test ArviZStats._printf_with_sigdigits(123.456, 3) == "123"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 3) == "-123"
+        @test ArviZStats._printf_with_sigdigits(123.456, 4) == "123.5"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 4) == "-123.5"
+        @test ArviZStats._printf_with_sigdigits(123.456, 5) == "123.46"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 5) == "-123.46"
+        @test ArviZStats._printf_with_sigdigits(123.456, 6) == "123.456"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 6) == "-123.456"
+        @test ArviZStats._printf_with_sigdigits(123.456, 7) == "123.4560"
+        @test ArviZStats._printf_with_sigdigits(-123.456, 7) == "-123.4560"
+        @test ArviZStats._printf_with_sigdigits(123.456, 8) == "123.45600"
+        @test ArviZStats._printf_with_sigdigits(0.00000123456, 1) == "1.e-06"
+        @test ArviZStats._printf_with_sigdigits(0.00000123456, 2) == "1.2e-06"
+    end
+
+    @testset "ft_printf_sigdigits" begin
+        @testset "all columns" begin
+            @testset for sigdigits in 1:5
+                ft1 = ArviZStats.ft_printf_sigdigits(sigdigits)
+                for i in 1:10, j in 1:5
+                    v = randn()
+                    @test ft1(v, i, j) == ArviZStats._printf_with_sigdigits(v, sigdigits)
+                    @test ft1("foo", i, j) == "foo"
+                end
+            end
+        end
+        @testset "subset of columns" begin
+            @testset for sigdigits in 1:5
+                ft = ArviZStats.ft_printf_sigdigits(sigdigits, [2, 3])
+                for i in 1:10, j in 1:5
+                    v = randn()
+                    if j ∈ [2, 3]
+                        @test ft(v, i, j) == ArviZStats._printf_with_sigdigits(v, sigdigits)
+                    else
+                        @test ft(v, i, j) === v
+                    end
+                    @test ft("foo", i, j) == "foo"
+                end
+            end
+        end
+    end
+
+    @testset "ft_printf_sigdigits_matching_se" begin
+        @testset "all columns" begin
+            @testset for scale in 1:3
+                se = rand(5)
+                ft = ArviZStats.ft_printf_sigdigits_matching_se(se; scale)
+                for i in eachindex(se), j in 1:5
+                    v = randn()
+                    sigdigits = ArviZStats.sigdigits_matching_se(v, se[i]; scale)
+                    @test ft(v, i, j) == ArviZStats._printf_with_sigdigits(v, sigdigits)
+                    @test ft("foo", i, j) == "foo"
+                end
+            end
+        end
+
+        @testset "subset of columns" begin
+            @testset for scale in 1:3
+                se = rand(5)
+                ft = ArviZStats.ft_printf_sigdigits_matching_se(se, [2, 3]; scale)
+                for i in eachindex(se), j in 1:5
+                    v = randn()
+                    if j ∈ [2, 3]
+                        sigdigits = ArviZStats.sigdigits_matching_se(v, se[i]; scale)
+                        @test ft(v, i, j) == ArviZStats._printf_with_sigdigits(v, sigdigits)
+                        @test ft("foo", i, j) == "foo"
+                    else
+                        @test ft(v, i, j) === v
+                    end
+                end
+            end
+        end
+    end
 end
