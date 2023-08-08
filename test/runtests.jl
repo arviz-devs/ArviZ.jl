63bb1ffafc33b2b9872faf5cdcf26c6576bdbb63
Seth Axen <seth@sethaxen.com>
2023-08-08 16:10:40 +0200

    Remove ArviZStats

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,6 @@ using Test
 @testset "ArviZ" begin
     include("helpers.jl")
     include("test_utils.jl")
-    include("ArviZStats/runtests.jl")
     include("test_samplechains.jl")
     include("test_mcmcchains.jl")
 end
