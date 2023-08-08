5d5a2d282d3c52c4b4d9fc01742aa92a595b7bb6
Seth Axen <seth@sethaxen.com>
2023-08-08 22:15:37 +0200

    Convert ArviZ.jl into a meta-package (#297)
    * Remove unneeded `@doc`
    
    * Remove all unneeded dependencies
    
    * Remove ArviZStats
    
    * Remove R installation from CI
    
    * Add PosteriorStats and Reexport as deps
    
    * Reexport component packages
    
    * Fix broken links
    
    * Update stats API docs
    
    * Add back PSIS compat bound
    
    * Remove unused test dependencies
    
    * Remove test Project.toml
    
    * Add missing SampleChainsDynamicHMC compat
    
    * Remove docs compat entry
    
    * Add component packages as docs deps
    
    This is needed so that extensions can load the package with `using SomePackage`
    
    * Correctly set up to build docstrings in extensions
    
    * Build docs strictly
    
    * Document meta-ness of package
    
    * Build docs nightly
    
    * Increment minor version number
    
    * Add StatsBase for doctests
    
    * Disable caching
    
    * Try building docs directly
    
    * Copy from InferenceObjects's docs build [no ci]
    
    * Revert "Copy from InferenceObjects's docs build [no ci]"
    
    This reverts commit cafadf8872140cf92515c5fd26fd0b48f62fc613.
    
    * Revert "Revert "Copy from InferenceObjects's docs build""
    
    This reverts commit 0cdfd99bcf7437f26a9f0b3e73d40090cef3489d.
    
    * Revert "Disable caching"
    
    This reverts commit 6ccbadfbd8bd6a4adcae67d0429e6123e0a9b10f.

--- src/ArviZ.jl
+++ src/ArviZ.jl
@@ -1,32 +1,11 @@
 module ArviZ
 
-using Base: @__doc__
-using REPL
-using OrderedCollections: OrderedDict
-using DimensionalData: DimensionalData, Dimensions
-using LogExpFunctions: logsumexp
+using Reexport
 
-import Base:
-    convert,
-    get,
-    getindex,
-    getproperty,
-    hash,
-    haskey,
-    iterate,
-    length,
-    propertynames,
-    setindex,
-    show,
-    write,
-    +
-import Base.Docs: getdoc
-using StatsBase: StatsBase
-import Markdown: @doc_str
-
-using InferenceObjects
-
-using MCMCDiagnosticTools:
+## Re-export component packages
+@reexport using InferenceObjects
+# only export recommended functions and corresponding utilities
+@reexport using MCMCDiagnosticTools:
     MCMCDiagnosticTools,
     AutocovMethod,
     FFTAutocovMethod,
@@ -37,46 +16,16 @@ using MCMCDiagnosticTools:
     mcse,
     rhat,
     rstar
-
-# Exports
-
-## Stats
-export ArviZStats
-export AbstractELPDResult, PSISLOOResult, WAICResult
-export PSIS, PSISResult, psis, psis!
-export elpd_estimates, information_criterion, loo, waic
-export AbstractModelWeightsMethod, BootstrappedPseudoBMA, PseudoBMA, Stacking, model_weights
-export ModelComparisonResult, compare
-export SummaryStats, summarystats
-export hdi, hdi!, loo_pit, r2_score
-
-## Diagnostics
-export MCMCDiagnosticTools, AutocovMethod, FFTAutocovMethod, BDAAutocovMethod
-export bfmi, ess, ess_rhat, mcse, rhat, rstar
-
-## InferenceObjects
-export InferenceObjects,
-    Dataset,
-    InferenceData,
-    convert_to_dataset,
-    convert_to_inference_data,
-    from_dict,
-    from_namedtuple,
-    namedtuple_to_dataset
-
-## NetCDF I/O
-export from_netcdf, to_netcdf
+@reexport using PosteriorStats
+@reexport using PSIS
+@reexport using StatsBase: summarystats
 
 ## Conversions
 export from_mcmcchains, from_samplechains
 
 const EXTENSIONS_SUPPORTED = isdefined(Base, :get_extension)
-const DEFAULT_SAMPLE_DIMS = Dimensions.key2dim((:chain, :draw))
 
 include("utils.jl")
-include("ArviZStats/ArviZStats.jl")
-using .ArviZStats
-
 include("conversions.jl")
 
 if !EXTENSIONS_SUPPORTED
