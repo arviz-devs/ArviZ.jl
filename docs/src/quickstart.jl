4f68c7c91b861ed83884d3e5d5d1aedafc0d5f2f
Seth Axen <seth@sethaxen.com>
2026-03-05 15:13:46 +0100

    docs: Reformat Stan model

--- docs/src/quickstart.jl
+++ docs/src/quickstart.jl
@@ -298,27 +298,24 @@ begin
       array[J] real y;
       array[J] real<lower=0> sigma;
     }
-
     parameters {
       real mu;
       real<lower=0> tau;
       array[J] real theta;
     }
-
     model {
       mu ~ normal(0, 5);
       tau ~ cauchy(0, 5);
       theta ~ normal(mu, tau);
       y ~ normal(theta, sigma);
     }
-
     generated quantities {
-        vector[J] log_lik;
-        vector[J] y_hat;
-        for (j in 1:J) {
-            log_lik[j] = normal_lpdf(y[j] | theta[j], sigma[j]);
-            y_hat[j] = normal_rng(theta[j], sigma[j]);
-        }
+      vector[J] log_lik;
+      vector[J] y_hat;
+      for (j in 1 : J) {
+        log_lik[j] = normal_lpdf(y[j] | theta[j], sigma[j]);
+        y_hat[j] = normal_rng(theta[j], sigma[j]);
+      }
     }
     """
 
