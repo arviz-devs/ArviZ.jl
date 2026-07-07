a7156f2bfab226715287f6adb7180337c877e952
Documenter.jl <documenter@juliadocs.github.io>
2026-07-07 11:45:14 +0000

    delete history

---
+++ previews/PR307/creating_custom_plots.jl
@@ -0,0 +1,285 @@
+### A Pluto.jl notebook ###
+# v0.19.16
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e5e15f39-027c-42aa-a765-588ec1cd0b63
+using Pkg, InteractiveUtils
+
+# ╔═╡ b1784f52-0cf2-11ed-32ba-69a8f63b48a9
+# Remove this cell to use release versions of dependencies
+# hideall
+let
+    docs_dir = dirname(@__DIR__)
+    pkg_dir = dirname(docs_dir)
+
+    Pkg.activate(docs_dir)
+    Pkg.develop(; path=pkg_dir)
+    Pkg.instantiate()
+end;
+
+# ╔═╡ 34ee7907-49e7-4e32-9bd5-1c5d490a13d8
+begin
+    using ArviZ, ArviZExampleData, DimensionalData, DataFrames, Statistics
+    using AlgebraOfGraphics, CairoMakie
+    using AlgebraOfGraphics: density
+    set_aog_theme!()
+end;
+
+# ╔═╡ 41b1ddca-20dc-4821-9cec-d7192ef34795
+using PlutoUI
+
+# ╔═╡ 70f66296-349c-48a5-9b83-cc5c6cdbd514
+md"""
+# Creating custom plots
+"""
+
+# ╔═╡ 47c7a1fd-0862-49d5-879d-d59d321ce014
+md"""
+While ArviZ includes many plotting functions for visualizing the data stored in `InferenceData` objects, you will often need to construct custom plots, or you may want to tweak some of our plots in your favorite plotting package.
+
+In this tutorial, we will show you a few useful techniques you can use to construct these plots using Julia's plotting packages.
+For demonstration purposes, we'll use Makie.jl and AlgebraOfGraphics.jl, which can consume `Dataset` objects since they implement the Tables interface.
+However, we could just as easily have used StatsPlots.jl.
+"""
+
+# ╔═╡ 7bdff7ba-408c-4893-9857-7cf328301a34
+md"""
+We'll start by loading some draws from an implementation of the non-centered parameterization of the 8 schools model.
+In this parameterization, the model has some sampling issues.
+"""
+
+# ╔═╡ cbd81558-e355-4e7a-ba51-0ba9299cb558
+idata = load_example_data("centered_eight")
+
+# ╔═╡ 898d83d1-cd3a-47a4-84b1-f2cf0f7bf959
+idata.posterior
+
+# ╔═╡ da4e989e-a08c-45af-8e55-20ac3ed58745
+md"""
+The plotting functions we'll be using interact with a tabular view of a `Dataset`.
+Let's see what that view looks like for a `Dataset`:
+"""
+
+# ╔═╡ 52ef5ef5-2ea8-4e1a-8cd5-ba3aa5da6d9b
+df = DataFrame(idata.posterior)
+
+# ╔═╡ e77c2bc3-8c23-446d-9ec4-3a155aea23e9
+md"""
+The tabular view includes dimensions and variables as columns.
+
+When variables with different dimensions are flattened into a tabular form, there's always some duplication of values.
+As a simple case, note that `chain`, `draw`, and `school` all have repeated values in the above table.
+
+In this case, `theta` has the `school` dimension, but `tau` doesn't, so the values of `tau` will be repeated in the table for each value of `school`.
+"""
+
+# ╔═╡ 0d625d42-726d-4cb3-8456-c54e1001df6d
+df[df.school .== Ref("Choate"), :].tau == df[df.school .== Ref("Deerfield"), :].tau
+
+# ╔═╡ 9b1a7256-c875-4266-b2fb-47c29f2b13c9
+md"""
+In our first example, this will be important.
+
+Here, let's construct a trace plot.
+Besides `idata`, all functions and types in the following cell are defined in AlgebraOfGraphics or Makie:
+- `data(...)` indicates that the wrapped object implements the Tables interface
+- `mapping` indicates how the data should be used. The symbols are all column names in the table, which for us are our variable names and dimensions.
+- `visual` specifies how the data should be converted to a plot.
+- `Lines` is a plot type defined in Makie.
+- `draw` takes this combination and plots it.
+"""
+
+# ╔═╡ 0f4483b1-5820-45e2-8812-14550bff69e2
+draw(
+    data(idata.posterior.mu) *
+    mapping(:draw, :mu; color=:chain => nonnumeric) *
+    visual(Lines; alpha=0.8),
+)
+
+# ╔═╡ ee4ab468-dbf6-46af-a7c5-44b82c031c2c
+md"""
+Note the line `idata.posterior.mu`.
+If we had just used `idata.posterior`, the plot would have looked more-or-less the same, but there would be artifacts due to `mu` being copied many times.
+By selecting `mu` directly, all other dimensions are discarded, so each value of `mu` appears in the plot exactly once.
+
+When examining an MCMC trace plot, we want to see a "fuzzy caterpillar".
+Instead we see a few places where the Markov chains froze.
+We can do the same for `theta` as well, but it's more useful here to separate these draws by `school`.
+"""
+
+# ╔═╡ 9b590c11-b15a-4199-8d9f-da8e1735fed2
+draw(
+    data(idata.posterior) *
+    mapping(:draw, :theta; layout=:school, color=:chain => nonnumeric) *
+    visual(Lines; alpha=0.8),
+)
+
+# ╔═╡ ca18a574-a5c6-4d83-a5d6-b5687f569522
+md"""
+Suppose we want to compare `tau` with `theta` for two different schools.
+To do so, we use `InferenceData`s indexing syntax to subset the data.
+"""
+
+# ╔═╡ 746785a2-c472-467f-973a-d2390ec3e0bb
+draw(
+    data(idata[:posterior, school=At(["Choate", "Deerfield"])]) *
+    mapping(:theta, :tau; color=:school) *
+    density() *
+    visual(Contour; levels=10),
+)
+
+# ╔═╡ e221c4b1-2256-4f97-bfd8-aeaf02fedc1b
+md"""
+We can also compare the density plots constructed from each chain for different schools.
+"""
+
+# ╔═╡ 663e5edb-a751-40e7-96ec-495685453515
+draw(
+    data(idata.posterior) *
+    mapping(:theta; layout=:school, color=:chain => nonnumeric) *
+    density(),
+)
+
+# ╔═╡ 6e10a67d-045a-4fa9-a7ad-90fa79015ea8
+md"""
+If we want to compare many schools in a single plot, an ECDF plot is more convenient.
+"""
+
+# ╔═╡ 9d8a23e1-4961-44c6-b272-afb8883df1d7
+draw(
+    data(idata.posterior) * mapping(:theta; color=:school => nonnumeric) * visual(ECDFPlot);
+    axis=(; ylabel="probability"),
+)
+
+# ╔═╡ 7068ad2c-2260-4afc-b85d-229b2be6c207
+md"""
+So far we've just plotted data from one group, but we often want to combine data from multiple groups in one plot.
+The simplest way to do this is to create the plot out of multiple layers.
+Here we use this approach to plot the observations over the posterior predictive distribution.
+"""
+
+# ╔═╡ c860bb8d-c15c-4ac9-b2b7-143f6f12bf0b
+draw(
+    (data(idata.posterior_predictive) * mapping(:obs; layout=:school) * density()) +
+    (data(idata.observed_data) * mapping(:obs, :obs => zero => ""; layout=:school)),
+)
+
+# ╔═╡ 6017222f-769b-4db7-9918-af70c792a158
+md"""
+Another option is to combine the groups into a single dataset.
+
+Here we compare the prior and posterior.
+Since the prior has 1 chain and the posterior has 4 chains, if we were to combine them into a table, the structure would need to be ragged.
+This is not currently supported.
+
+We can then either plot the two distributions separately as we did before, or we can compare a single chain from each group.
+This is what we'll do here.
+To concatenate the two groups, we introduce a new named dimension using `DimensionalData.Dim`.
+"""
+
+# ╔═╡ b072dd93-3076-4630-bb2a-b9d490045a78
+draw(
+    data(
+        cat(
+            idata.posterior[chain=[1]], idata.prior; dims=Dim{:group}([:posterior, :prior])
+        )[:mu],
+    ) *
+    mapping(:mu; color=:group) *
+    histogram(; bins=20) *
+    visual(; alpha=0.8);
+    axis=(; ylabel="probability"),
+)
+
+# ╔═╡ 82229ba7-101d-4fbc-96a9-ca56d899ebd5
+md"""
+From the trace plots, we suspected the geometry of this posterior was bad.
+Let's highlight divergent transitions.
+To do so, we merge `posterior` and `samplestats`, which can do with `merge` since they share no common variable names.
+"""
+
+# ╔═╡ 2a47f53a-a054-426d-b536-ccffcf62dd15
+draw(
+    data(merge(idata.posterior, idata.sample_stats)) * mapping(
+        :theta,
+        :tau;
+        layout=:school,
+        color=:diverging,
+        markersize=:diverging => (d -> d ? 5 : 2),
+    ),
+)
+
+# ╔═╡ 3c939c5a-90c6-4367-9a9b-2525796425ce
+md"""
+When we try building more complex plots, we may need to build new `Dataset`s from our existing ones.
+
+One example of this is the corner plot.
+To build this plot, we need to make a copy of `theta` with a copy of the `school` dimension.
+"""
+
+# ╔═╡ b3c8eed9-4083-457e-89dd-71d678b724ef
+let
+    theta = idata.posterior.theta[school=1:4]
+    theta2 = rebuild(set(theta; school=:school2); name=:theta2)
+    plot_data = Dataset(theta, theta2, idata.sample_stats.diverging)
+    draw(
+        data(plot_data) * mapping(
+            :theta,
+            :theta2 => "theta";
+            col=:school,
+            row=:school2,
+            color=:diverging,
+            markersize=:diverging => (d -> d ? 3 : 1),
+        );
+        figure=(; figsize=(5, 5)),
+        axis=(; aspect=1),
+    )
+end
+
+# ╔═╡ 7c12ac28-694c-4c12-af00-2e74bad04683
+md"""
+## Environment
+"""
+
+# ╔═╡ ab01574e-0b29-4325-b42e-9a360267ba95
+with_terminal(Pkg.status; color=false)
+
+# ╔═╡ e3774f8d-9885-4d49-8503-f3fc94b8b113
+with_terminal(versioninfo)
+
+# ╔═╡ Cell order:
+# ╟─70f66296-349c-48a5-9b83-cc5c6cdbd514
+# ╠═b1784f52-0cf2-11ed-32ba-69a8f63b48a9
+# ╟─47c7a1fd-0862-49d5-879d-d59d321ce014
+# ╠═34ee7907-49e7-4e32-9bd5-1c5d490a13d8
+# ╟─7bdff7ba-408c-4893-9857-7cf328301a34
+# ╠═cbd81558-e355-4e7a-ba51-0ba9299cb558
+# ╠═898d83d1-cd3a-47a4-84b1-f2cf0f7bf959
+# ╟─da4e989e-a08c-45af-8e55-20ac3ed58745
+# ╠═52ef5ef5-2ea8-4e1a-8cd5-ba3aa5da6d9b
+# ╟─e77c2bc3-8c23-446d-9ec4-3a155aea23e9
+# ╠═0d625d42-726d-4cb3-8456-c54e1001df6d
+# ╟─9b1a7256-c875-4266-b2fb-47c29f2b13c9
+# ╠═0f4483b1-5820-45e2-8812-14550bff69e2
+# ╟─ee4ab468-dbf6-46af-a7c5-44b82c031c2c
+# ╠═9b590c11-b15a-4199-8d9f-da8e1735fed2
+# ╟─ca18a574-a5c6-4d83-a5d6-b5687f569522
+# ╠═746785a2-c472-467f-973a-d2390ec3e0bb
+# ╟─e221c4b1-2256-4f97-bfd8-aeaf02fedc1b
+# ╠═663e5edb-a751-40e7-96ec-495685453515
+# ╟─6e10a67d-045a-4fa9-a7ad-90fa79015ea8
+# ╠═9d8a23e1-4961-44c6-b272-afb8883df1d7
+# ╟─7068ad2c-2260-4afc-b85d-229b2be6c207
+# ╠═c860bb8d-c15c-4ac9-b2b7-143f6f12bf0b
+# ╟─6017222f-769b-4db7-9918-af70c792a158
+# ╠═b072dd93-3076-4630-bb2a-b9d490045a78
+# ╟─82229ba7-101d-4fbc-96a9-ca56d899ebd5
+# ╠═2a47f53a-a054-426d-b536-ccffcf62dd15
+# ╟─3c939c5a-90c6-4367-9a9b-2525796425ce
+# ╠═b3c8eed9-4083-457e-89dd-71d678b724ef
+# ╟─7c12ac28-694c-4c12-af00-2e74bad04683
+# ╠═e5e15f39-027c-42aa-a765-588ec1cd0b63
+# ╠═41b1ddca-20dc-4821-9cec-d7192ef34795
+# ╠═ab01574e-0b29-4325-b42e-9a360267ba95
+# ╠═e3774f8d-9885-4d49-8503-f3fc94b8b113
---
+++ v0.8.0/quickstart.jl
@@ -0,0 +1,441 @@
+### A Pluto.jl notebook ###
+# v0.19.16
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2c0f3d74-8b1e-4648-b0fa-f8050035b0fd
+using Pkg, InteractiveUtils
+
+# ╔═╡ ac57d957-0bdd-457a-ac15-9a4f94f0c785
+# Remove this cell to use release versions of dependencies 
+# hideall
+let
+    docs_dir = dirname(@__DIR__)
+    pkg_dir = dirname(docs_dir)
+
+    Pkg.activate(docs_dir)
+    Pkg.develop(; path=pkg_dir)
+    Pkg.instantiate()
+end;
+
+# ╔═╡ 467c2d13-6bfe-4feb-9626-fb14796168aa
+using ArviZ, CmdStan, Distributions, LinearAlgebra, PyPlot, Random, Turing
+
+# ╔═╡ 56a39a90-0594-48f4-ba04-f7b612019cd1
+using PlutoUI
+
+# ╔═╡ a23dfd65-50a8-4872-8c41-661e96585aca
+md"""
+# [ArviZ.jl Quickstart](#quickstart)
+
+!!! note
+    
+    This tutorial is adapted from [ArviZ's quickstart](https://python.arviz.org/en/latest/getting_started/Introduction.html).
+"""
+
+# ╔═╡ d2eedd48-48c6-4fcd-b179-6be7fe68d3d6
+md"""
+## [Setup](#setup)
+
+Here we add the necessary packages for this notebook and load a few we will use throughout.
+"""
+
+# ╔═╡ 06b00794-e97f-472b-b526-efe4815103f8
+# ArviZ ships with style sheets!
+ArviZ.use_style("arviz-darkgrid")
+
+# ╔═╡ 5acbfd9a-cfea-4c3c-a3f0-1744eb7e4e27
+md"""
+## [Get started with plotting](#Get-started-with-plotting)
+
+ArviZ.jl is designed to be used with libraries like [CmdStan](https://github.com/StanJulia/CmdStan.jl), [Turing.jl](https://turing.ml), and [Soss.jl](https://github.com/cscherrer/Soss.jl) but works fine with raw arrays.
+"""
+
+# ╔═╡ efb3f0af-9fac-48d8-bbb2-2dd6ebd5e4f6
+rng1 = Random.MersenneTwister(37772);
+
+# ╔═╡ 401e9b91-0bca-4369-8d36-3d9f0b3ad60b
+begin
+    plot_posterior(randn(rng1, 100_000))
+    gcf()
+end
+
+# ╔═╡ 2c718ea5-2800-4df6-b62c-e0a9e440a1c3
+md"""
+Plotting a dictionary of arrays, ArviZ.jl will interpret each key as the name of a different random variable.
+Each row of an array is treated as an independent series of draws from the variable, called a _chain_.
+Below, we have 10 chains of 50 draws each for four different distributions.
+"""
+
+# ╔═╡ 49f19c17-ac1d-46b5-a655-4376b7713244
+let
+    s = (50, 10)
+    plot_forest((
+        normal=randn(rng1, s),
+        gumbel=rand(rng1, Gumbel(), s),
+        student_t=rand(rng1, TDist(6), s),
+        exponential=rand(rng1, Exponential(), s),
+    ),)
+    gcf()
+end
+
+# ╔═╡ a9789109-2b90-40f7-926c-e7c87025d15f
+md"""
+## [Plotting with MCMCChains.jl's `Chains` objects produced by Turing.jl](#Plotting-with-MCMCChains.jl's-Chains-objects-produced-by-Turing.jl)
+
+ArviZ is designed to work well with high dimensional, labelled data.
+Consider the [eight schools model](https://statmodeling.stat.columbia.edu/2014/01/21/everything-need-know-bayesian-statistics-learned-eight-schools/), which roughly tries to measure the effectiveness of SAT classes at eight different schools.
+To show off ArviZ's labelling, I give the schools the names of [a different eight schools](https://en.wikipedia.org/wiki/Eight_Schools_Association).
+
+This model is small enough to write down, is hierarchical, and uses labelling.
+Additionally, a centered parameterization causes [divergences](https://mc-stan.org/users/documentation/case-studies/divergences_and_bias.html) (which are interesting for illustration).
+
+First we create our data and set some sampling parameters.
+"""
+
+# ╔═╡ 69124a94-a6aa-43f6-8d4f-fa9a6b1cfef1
+begin
+    J = 8
+    y = [28.0, 8.0, -3.0, 7.0, -1.0, 1.0, 18.0, 12.0]
+    σ = [15.0, 10.0, 16.0, 11.0, 9.0, 11.0, 10.0, 18.0]
+    schools = [
+        "Choate",
+        "Deerfield",
+        "Phillips Andover",
+        "Phillips Exeter",
+        "Hotchkiss",
+        "Lawrenceville",
+        "St. Paul's",
+        "Mt. Hermon",
+    ]
+    ndraws = 1_000
+    ndraws_warmup = 1_000
+    nchains = 4
+end;
+
+# ╔═╡ 10986e0e-55f7-40ea-ab63-274fbab3126d
+md"""
+Now we write and run the model using Turing:
+"""
+
+# ╔═╡ f383d541-e22d-44b4-b8cb-28b3d67944a1
+Turing.@model function model_turing(y, σ, J=length(y))
+    μ ~ Normal(0, 5)
+    τ ~ truncated(Cauchy(0, 5), 0, Inf)
+    θ ~ filldist(Normal(μ, τ), J)
+    for i in 1:J
+        y[i] ~ Normal(θ[i], σ[i])
+    end
+end
+
+# ╔═╡ 86cb5e19-49e4-4e5e-8b89-e76936932055
+rng2 = Random.MersenneTwister(16653);
+
+# ╔═╡ 85bbcba7-c0f9-4c86-9cdf-a27055d3d448
+begin
+    param_mod_turing = model_turing(y, σ)
+    sampler = NUTS(ndraws_warmup, 0.8)
+
+    turing_chns = Turing.sample(
+        rng2, model_turing(y, σ), sampler, MCMCThreads(), ndraws, nchains
+    )
+end;
+
+# ╔═╡ bd4ab044-51ce-4af9-83b2-bd8fc827f810
+md"""
+Most ArviZ functions work fine with `Chains` objects from Turing:
+"""
+
+# ╔═╡ 500f4e0d-0a36-4b5c-8900-667560fbf1d4
+begin
+    plot_autocorr(turing_chns; var_names=(:μ, :τ))
+    gcf()
+end
+
+# ╔═╡ 1129ad94-f65a-4332-b354-21bcf7e53541
+md"""
+### Convert to `InferenceData`
+
+For much more powerful querying, analysis and plotting, we can use built-in ArviZ utilities to convert `Chains` objects to multidimensional data structures with named dimensions and indices.
+Note that for such dimensions, the information is not contained in `Chains`, so we need to provide it.
+
+ArviZ is built to work with [`InferenceData`](https://julia.arviz.org/stable/reference/#ArviZ.InferenceData), and the more *groups* it has access to, the more powerful analyses it can perform.
+"""
+
+# ╔═╡ 803efdd8-656e-4e37-ba36-81195d064972
+idata_turing_post = from_mcmcchains(
+    turing_chns;
+    coords=(; school=schools),
+    dims=NamedTuple(k => (:school,) for k in (:y, :σ, :θ)),
+    library="Turing",
+)
+
+# ╔═╡ 79f342c8-0738-432b-bfd7-2da25e50fa91
+md"""
+Each group is an [`ArviZ.Dataset`](https://julia.arviz.org/stable/reference/#ArviZ.Dataset), a `DimensionalData.AbstractDimStack` that can be used identically to a [`DimensionalData.Dimstack`](https://rafaqz.github.io/DimensionalData.jl/stable/api/#DimensionalData.DimStack).
+We can view a summary of the dataset.
+"""
+
+# ╔═╡ 6209f947-5001-4507-b3e8-9747256f3328
+idata_turing_post.posterior
+
+# ╔═╡ 26692722-db0d-41de-b58c-339b639f6948
+md"""
+Here is a plot of the trace. Note the intelligent labels.
+"""
+
+# ╔═╡ 14046b83-9c0a-4d33-ae4e-36c7d6f1b2e6
+begin
+    plot_trace(idata_turing_post)
+    gcf()
+end
+
+# ╔═╡ 737f319c-1ddd-45f2-8d10-aaecdc1334be
+md"We can also generate summary stats..."
+
+# ╔═╡ 5be3131d-db8b-44c8-9fcc-571d68695148
+summarystats(idata_turing_post)
+
+# ╔═╡ 0787a842-63ca-407d-9d07-a3d949940f92
+md"...and examine the energy distribution of the Hamiltonian sampler."
+
+# ╔═╡ 6e8343c8-bee3-4e1d-82d6-1885bfd1dbec
+begin
+    plot_energy(idata_turing_post)
+    gcf()
+end
+
+# ╔═╡ cba6f6c9-82c4-4957-acc3-36e9f1c95d76
+md"""
+### Additional information in Turing.jl
+
+With a few more steps, we can use Turing to compute additional useful groups to add to the `InferenceData`.
+
+To sample from the prior, one simply calls `sample` but with the `Prior` sampler:
+"""
+
+# ╔═╡ 79374a8b-98c1-443c-acdd-0ee00bd42b38
+prior = Turing.sample(rng2, param_mod_turing, Prior(), ndraws);
+
+# ╔═╡ c23562ba-3b0b-49d9-bb41-2fedaa9e9500
+md"""
+To draw from the prior and posterior predictive distributions we can instantiate a "predictive model", i.e. a Turing model but with the observations set to `missing`, and then calling `predict` on the predictive model and the previously drawn samples:
+"""
+
+# ╔═╡ d0b447fe-26b2-48e0-bcec-d9f04697973a
+begin
+    # Instantiate the predictive model
+    param_mod_predict = model_turing(similar(y, Missing), σ)
+    # and then sample!
+    prior_predictive = Turing.predict(rng2, param_mod_predict, prior)
+    posterior_predictive = Turing.predict(rng2, param_mod_predict, turing_chns)
+end;
+
+# ╔═╡ 4d2fdcbe-c1d4-43b6-b382-f2e956b952a1
+md"""
+And to extract the pointwise log-likelihoods, which is useful if you want to compute metrics such as [`loo`](https://julia.arviz.org/stable/reference/#ArviZ.loo),
+"""
+
+# ╔═╡ 5a075722-232f-40fc-a499-8dc5b0c2424a
+log_likelihood = let
+    log_likelihood = Turing.pointwise_loglikelihoods(
+        param_mod_turing, MCMCChains.get_sections(turing_chns, :parameters)
+    )
+    # Ensure the ordering of the loglikelihoods matches the ordering of `posterior_predictive`
+    ynames = string.(keys(posterior_predictive))
+    log_likelihood_y = getindex.(Ref(log_likelihood), ynames)
+    (; y=cat(log_likelihood_y...; dims=3))
+end;
+
+# ╔═╡ 1b5af2c3-f2ce-4e9d-9ad7-ac287a9178e2
+md"This can then be included in the [`from_mcmcchains`](https://julia.arviz.org/stable/reference/#ArviZ.from_mcmcchains) call from above:"
+
+# ╔═╡ b38c7a43-f00c-43c0-aa6b-9c581d6d0c73
+idata_turing = from_mcmcchains(
+    turing_chns;
+    posterior_predictive,
+    log_likelihood,
+    prior,
+    prior_predictive,
+    observed_data=(; y),
+    coords=(; school=schools),
+    dims=NamedTuple(k => (:school,) for k in (:y, :σ, :θ)),
+    library=Turing,
+)
+
+# ╔═╡ a3b71e4e-ac1f-4404-b8a2-35d03d326774
+md"""
+Then we can for example compute the expected *leave-one-out (LOO)* predictive density, which is an estimate of the out-of-distribution predictive fit of the model:
+"""
+
+# ╔═╡ f552b5b5-9744-41df-af90-46405367ea0b
+loo(idata_turing; pointwise=false) # higher is better
+
+# ╔═╡ 9d3673f5-b57b-432e-944a-70b23643128a
+md"""
+If the model is well-calibrated, i.e. it replicates the true generative process well, the CDF of the pointwise LOO values should be similarly distributed to a uniform distribution.
+This can be inspected visually:
+"""
+
+# ╔═╡ 05c9be29-7758-4324-971c-5579f99aaf9d
+begin
+    plot_loo_pit(idata_turing; y=:y, ecdf=true)
+    gcf()
+end
+
+# ╔═╡ 98acc304-22e3-4e6b-a2f4-d22f6847145b
+md"""
+## [Plotting with CmdStan.jl outputs](#Plotting-with-CmdStan.jl-outputs)
+
+CmdStan.jl and StanSample.jl also default to producing `Chains` outputs, and we can easily plot these chains.
+
+Here is the same centered eight schools model:
+"""
+
+# ╔═╡ b46af168-1ce3-4058-a014-b66c645a6e0d
+begin
+    schools_code = """
+    data {
+      int<lower=0> J;
+      real y[J];
+      real<lower=0> sigma[J];
+    }
+
+    parameters {
+      real mu;
+      real<lower=0> tau;
+      real theta[J];
+    }
+
+    model {
+      mu ~ normal(0, 5);
+      tau ~ cauchy(0, 5);
+      theta ~ normal(mu, tau);
+      y ~ normal(theta, sigma);
+    }
+
+    generated quantities {
+        vector[J] log_lik;
+        vector[J] y_hat;
+        for (j in 1:J) {
+            log_lik[j] = normal_lpdf(y[j] | theta[j], sigma[j]);
+            y_hat[j] = normal_rng(theta[j], sigma[j]);
+        }
+    }
+    """
+
+    schools_data = Dict("J" => J, "y" => y, "sigma" => σ)
+    stan_chns = mktempdir() do path
+        stan_model = Stanmodel(;
+            model=schools_code,
+            name="schools",
+            nchains,
+            num_warmup=ndraws_warmup,
+            num_samples=ndraws,
+            output_format=:mcmcchains,
+            random=CmdStan.Random(28983),
+            tmpdir=path,
+        )
+        _, chns, _ = stan(stan_model, schools_data; summary=false)
+        return chns
+    end
+end;
+
+# ╔═╡ ab145e41-b230-4cad-bef5-f31e0e0770d4
+begin
+    plot_density(stan_chns; var_names=(:mu, :tau))
+    gcf()
+end
+
+# ╔═╡ ffc7730c-d861-48e8-b173-b03e0542f32b
+md"""
+Again, converting to `InferenceData`, we can get much richer labelling and mixing of data.
+Note that we're using the same [`from_cmdstan`](https://julia.arviz.org/stable/reference/#ArviZ.from_cmdstan) function used by ArviZ to process cmdstan output files, but through the power of dispatch in Julia, if we pass a `Chains` object, it instead uses ArviZ.jl's overloads, which forward to `from_mcmcchains`.
+"""
+
+# ╔═╡ 020cbdc0-a0a2-4d20-838f-c99b541d5832
+idata_stan = from_cmdstan(
+    stan_chns;
+    posterior_predictive=:y_hat,
+    observed_data=(; y),
+    log_likelihood=:log_lik,
+    coords=(; school=schools),
+    dims=NamedTuple(k => (:school,) for k in (:y, :sigma, :theta, :log_lik, :y_hat)),
+)
+
+# ╔═╡ e44b260c-9d2f-43f8-a64b-04245a0a5658
+md"""Here is a plot showing where the Hamiltonian sampler had divergences:"""
+
+# ╔═╡ 5070bbbc-68d2-49b8-bd91-456dc0da4573
+begin
+    plot_pair(
+        idata_stan;
+        coords=Dict(:school => ["Choate", "Deerfield", "Phillips Andover"]),
+        divergences=true,
+    )
+    gcf()
+end
+
+# ╔═╡ ac2b4378-bd1c-4164-af05-d9a35b1bb08f
+md"## [Environment](#environment)"
+
+# ╔═╡ fd84237a-7a19-4bbb-a702-faa31075ecbc
+with_terminal(Pkg.status; color=false)
+
+# ╔═╡ ad29b7f3-6f5e-4b04-bf70-2308a7d110c0
+with_terminal(versioninfo)
+
+# ╔═╡ Cell order:
+# ╟─a23dfd65-50a8-4872-8c41-661e96585aca
+# ╟─d2eedd48-48c6-4fcd-b179-6be7fe68d3d6
+# ╠═ac57d957-0bdd-457a-ac15-9a4f94f0c785
+# ╠═467c2d13-6bfe-4feb-9626-fb14796168aa
+# ╠═06b00794-e97f-472b-b526-efe4815103f8
+# ╟─5acbfd9a-cfea-4c3c-a3f0-1744eb7e4e27
+# ╠═efb3f0af-9fac-48d8-bbb2-2dd6ebd5e4f6
+# ╠═401e9b91-0bca-4369-8d36-3d9f0b3ad60b
+# ╟─2c718ea5-2800-4df6-b62c-e0a9e440a1c3
+# ╠═49f19c17-ac1d-46b5-a655-4376b7713244
+# ╟─a9789109-2b90-40f7-926c-e7c87025d15f
+# ╠═69124a94-a6aa-43f6-8d4f-fa9a6b1cfef1
+# ╟─10986e0e-55f7-40ea-ab63-274fbab3126d
+# ╠═f383d541-e22d-44b4-b8cb-28b3d67944a1
+# ╠═86cb5e19-49e4-4e5e-8b89-e76936932055
+# ╠═85bbcba7-c0f9-4c86-9cdf-a27055d3d448
+# ╟─bd4ab044-51ce-4af9-83b2-bd8fc827f810
+# ╠═500f4e0d-0a36-4b5c-8900-667560fbf1d4
+# ╟─1129ad94-f65a-4332-b354-21bcf7e53541
+# ╠═803efdd8-656e-4e37-ba36-81195d064972
+# ╟─79f342c8-0738-432b-bfd7-2da25e50fa91
+# ╠═6209f947-5001-4507-b3e8-9747256f3328
+# ╟─26692722-db0d-41de-b58c-339b639f6948
+# ╠═14046b83-9c0a-4d33-ae4e-36c7d6f1b2e6
+# ╟─737f319c-1ddd-45f2-8d10-aaecdc1334be
+# ╠═5be3131d-db8b-44c8-9fcc-571d68695148
+# ╟─0787a842-63ca-407d-9d07-a3d949940f92
+# ╠═6e8343c8-bee3-4e1d-82d6-1885bfd1dbec
+# ╟─cba6f6c9-82c4-4957-acc3-36e9f1c95d76
+# ╠═79374a8b-98c1-443c-acdd-0ee00bd42b38
+# ╟─c23562ba-3b0b-49d9-bb41-2fedaa9e9500
+# ╠═d0b447fe-26b2-48e0-bcec-d9f04697973a
+# ╟─4d2fdcbe-c1d4-43b6-b382-f2e956b952a1
+# ╠═5a075722-232f-40fc-a499-8dc5b0c2424a
+# ╟─1b5af2c3-f2ce-4e9d-9ad7-ac287a9178e2
+# ╠═b38c7a43-f00c-43c0-aa6b-9c581d6d0c73
+# ╟─a3b71e4e-ac1f-4404-b8a2-35d03d326774
+# ╠═f552b5b5-9744-41df-af90-46405367ea0b
+# ╟─9d3673f5-b57b-432e-944a-70b23643128a
+# ╠═05c9be29-7758-4324-971c-5579f99aaf9d
+# ╟─98acc304-22e3-4e6b-a2f4-d22f6847145b
+# ╠═b46af168-1ce3-4058-a014-b66c645a6e0d
+# ╠═ab145e41-b230-4cad-bef5-f31e0e0770d4
+# ╟─ffc7730c-d861-48e8-b173-b03e0542f32b
+# ╠═020cbdc0-a0a2-4d20-838f-c99b541d5832
+# ╟─e44b260c-9d2f-43f8-a64b-04245a0a5658
+# ╠═5070bbbc-68d2-49b8-bd91-456dc0da4573
+# ╟─ac2b4378-bd1c-4164-af05-d9a35b1bb08f
+# ╠═56a39a90-0594-48f4-ba04-f7b612019cd1
+# ╠═2c0f3d74-8b1e-4648-b0fa-f8050035b0fd
+# ╠═fd84237a-7a19-4bbb-a702-faa31075ecbc
+# ╠═ad29b7f3-6f5e-4b04-bf70-2308a7d110c0
